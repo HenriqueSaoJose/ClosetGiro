a7955f55333dd2a26d78819c28b6ee99b70fda84
HenriqueSaoJose <henriqueandradesj@outlook.com>
2026-03-18 18:13:21 -0300

    screen enviar sacola

--- app_closet/app/screens/sacola-pull.tsx
+++ app_closet/app/screens/sacola-pull.tsx
@@ -1,5 +1,6 @@
 import { ThemedText } from "@/components/themed-text";
-import { StyleSheet } from "react-native";
+import { Pressable, StyleSheet } from "react-native";
+import { Ionicons } from '@expo/vector-icons';
 import { ThemedView } from "@/components/themed-view";
 
 
@@ -7,8 +8,12 @@ export default function SacolaPullScreen(){
   return(
     <ThemedView style={styles.ContainerMain}>
           <ThemedView style={styles.ContainerMenu}>
-            <ThemedText>Sacolas Enviada</ThemedText>
-            <ThemedText>Estoque</ThemedText>
+            <ThemedView style={styles.ContainerText}>
+              <ThemedText style={styles.fontmain}>Sacolas Enviada</ThemedText>
+            </ThemedView>
+            <Pressable style={styles.buttonEstoque}>
+              <Ionicons name="arrow-down-circle-outline" size={35} color="white" />
+            </Pressable>
         </ThemedView>
         <ThemedView>
 
@@ -25,8 +30,42 @@ const styles = StyleSheet.create({
         justifyContent:'flex-start',
     },
     ContainerMenu:{
+        height: 60,
+        width: '90%',
         display: 'flex',
-        justifyContent: 'space-between',
-        alignItems:'flex-start'
+        flexDirection: 'row',
+        gap:"35%",
+        justifyContent: 'flex-start',
+        alignItems:'center',
+    },
+    ContainerText : {
+      height:"93%",
+      width: '50%',
+      backgroundColor: "#C76A8A",
+      display: 'flex',
+      alignItems: 'flex-start',
+      borderRadius: 5,
+      marginTop:5,
+      justifyContent: 'center',
+      paddingHorizontal:10,
+      shadowColor: "#000",
+      shadowOffset: { width: 4, height: 8 },
+      shadowOpacity: 0.27,
+      shadowRadius: 19,
+      elevation: 5,
+    },
+    fontmain:{
+      fontFamily: 'Italianno_400Regular',
+      color: "#ffff",
+      fontSize:30
+    },
+    buttonEstoque:{
+      width: '15%',
+      height:'86%',
+      backgroundColor: "#7C3AED",
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderRadius:100,
     }
 })
