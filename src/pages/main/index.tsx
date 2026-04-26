8f7b3df75708a2b62768e88514a7d335c50381c0
EvandroJosias <evandro.josias@yahoo.com.br>
2026-04-26 14:22:59 -0300

    Projeto Inicia em Branco

---
+++ src/pages/main/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text } from "react-native";
+import { style } from "./styles";
+
+export default function Main (){
+    return(
+        <View style={style.container}>
+            <Text>Main</Text>
+        </View>
+    )
+}
